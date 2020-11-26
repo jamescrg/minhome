4770fff176b13b5b910286b4109ac1856ea2f074
James <james8900@protonmail.com>
2020-11-26 21:16:07 +0000

    added theme column to users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->string('google_token');
+            $table->string('theme');
             $table->timestamps();
         });
     }
