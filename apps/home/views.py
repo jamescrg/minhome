df161df315f0fd65e937297bbc5e1c1dc8361ac2
James Craig <jamespcraig@gmail.com>
2022-09-08 12:57:44 -0400

    Revert "home: swapped out pytz for dateutil.tz"
    This reverts commit f9c4dfefb4eb6af311ddce63551f48ae42f57783.

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime, date, timedelta
-from dateutil import tz
+import pytz
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
@@ -29,7 +29,7 @@ def index(request):
     if not show_events:
 
         # get current day
-        now = datetime.now(tz.gettz('US/Eastern'))
+        now = datetime.now(pytz.timezone('US/Eastern'))
         today = now.date()
 
         # get day events were previously hidden
@@ -62,7 +62,7 @@ def index(request):
     if not show_tasks:
 
         # get current day
-        now = datetime.now(tz.gettz('US/Eastern'))
+        now = datetime.now(pytz.timezone('US/Eastern'))
         today = now.date()
 
         # get day events were previously hidden
