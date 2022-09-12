d30eb2a2035bed98bc7180f0369e6cbe61fea0e8
James Craig <jamespcraig@gmail.com>
2022-09-12 09:51:03 -0400

    tasks: set default value of status to 0 to ensure correct sort order

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -10,7 +10,7 @@ class Task(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     folder = models.ForeignKey(Folder, on_delete=models.SET_NULL, blank=True, null=True)
     title = models.CharField(max_length=200, null=True)
-    status = models.IntegerField(blank=True, null=True)
+    status = models.IntegerField(blank=True, null=True, default=0)
 
     def __str__(self):
         return f'{self.title} : {self.id}'
