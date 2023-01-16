5d8e27a598cfc28aefa948b1e28f01accfb9c812
James Craig <jamespcraig@gmail.com>
2023-01-16 08:42:43 -0500

    migrated database to postgres

--- config/settings.py
+++ config/settings.py
@@ -95,12 +95,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'old': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': settings_local.DB_NAME,
         'USER': settings_local.DB_USER,
         'PASSWORD': settings_local.DB_PASSWORD,
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': settings_local.PG_DB_NAME,
+        'USER': settings_local.PG_DB_USER,
+        'PASSWORD': settings_local.PG_DB_PASSWORD,
+        'HOST': 'localhost',
     }
+
 }
 
 
