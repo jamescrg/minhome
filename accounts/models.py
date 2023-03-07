13df418eeb9adc9b77f0c48029ee28706296c6d2
James Craig <jamespcraig@gmail.com>
2023-03-07 14:54:17 +0000

    Added a user settings field

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,4 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     google_credentials = models.TextField(null=True, blank=True)
+    settings = models.JSONField(default=dict, blank=True)
