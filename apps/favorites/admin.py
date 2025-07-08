a84f1f5326de39b12601a7e6f557465d0d631049
James Craig <jamespcraig@gmail.com>
2025-07-08 01:33:40 +0000

    favorites: admin.py file created

--- apps/favorites/admin.py
+++ apps/favorites/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Favorite
 
-# Register your models here.
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "folder", "name", "home_rank")
+
+
+admin.site.register(Favorite, FavoriteAdmin)
